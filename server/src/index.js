0e55602d400cc977c2b7875d2f2fe91b25b6320c
Marcos331305 <139604169+Marcos331305@users.noreply.github.com>
2025-07-11 10:42:28 +0530

    Initial server creation and backend folders structuring

---
+++ server/src/index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config(); // load environment variables from .env file before doing anything else
+import express from "express";
+
+const PORT = process.env.PORT || 5500;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT : ${PORT}`);
+});
